32d6f9ff577d28c58c31546c5b6ee40d36ab17f8
VUTHANHUET <71000712+VUTHANHUET@users.noreply.github.com>
2021-05-12 20:55:58 +0700

    update Flatlist

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, TouchableOpacity,FlatList, ScrollView } from 'react-native';
 import CategoryListItem from './components/CategoryListItem';
-export default function App() {
-  return (
-    <View style={styles.container}>
-     <CategoryListItem/>
-     <CategoryListItem/>
-     <CategoryListItem/>
-     <CategoryListItem/>
-     <CategoryListItem/>
-     <CategoryListItem/>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      categories: [
+        { id: 1, name: 'Dụng cụ trượt tuyết' },
+        { id: 2, name: 'Quần áo trượt tuyết' },
+        { id: 3, name: 'Kính mũ' }
+      ]
+    }
+  };
+
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <FlatList 
+        data = {categories}
+        renderItem = {({ item }) => <CategoryListItem category={item}/> }
+        keyExtractor = { item  => `${item.id}`}
+        contentContainerStyle = {{paddingLeft: 16, paddingRight: 16, paddingTop: 50}}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -21,7 +35,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'stretch',
     justifyContent: 'center',
-    paddingLeft: 30,
-    paddingRight:30
+    paddingLeft: 16,
+    paddingRight:16
   },
 });
--- components/CategoryListItem.js
+++ components/CategoryListItem.js
@@ -7,24 +7,29 @@ import {
 } from 'react-native' 
 import SkiImage from '../assets/ski.png'
 export default function CategoryListItem(props){
-    return <View style={styles.container}>
-        <Text style={styles.title}>  CategoryListItem </Text>
+    const { category } = props;
+    return (
+    <View style={styles.container}>
+        <Text style={styles.title}> {category.name} </Text>
         <Image style={styles.categoryImage} source={SkiImage}/>
     </View>
+    );
 }
 
 const styles = StyleSheet.create ({
     container: {
         alignItems: 'center',
         padding: 16,
-        borderRadius: 4,
+        borderRadius: 10,
         backgroundColor: '#FFF',
         shadowColor: '#000',
-        shadowOpacity: 0.3,
+        shadowOpacity: 0.8,
         shadowRadius: 10,
         shadowOffset: { width: 0, height: 0},
         marginBottom: 16,
-        elevation:2
+        elevation:7,
+        marginLeft: 3,
+        marginRight: 3
     },
     categoryImage: {
         width: 64,
